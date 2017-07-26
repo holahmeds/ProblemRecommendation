e001fe5e50c81f96ba00a0702a3a320ba5751130
Ahmed Rafiq <ahmedrafiq47@gmail.com>
2017-07-26 14:52:06 +0600

    We now also store problem ids
    This will make it easier to get a user's accepted problems since we won't have to lookup
    the problem number for each accepted solution.

--- src/main/java/com/strawhats/problemrecommendation/Problem.java
+++ src/main/java/com/strawhats/problemrecommendation/Problem.java
@@ -2,26 +2,37 @@ package com.strawhats.problemrecommendation;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONObject;
 
 public class Problem {
 
 	private static final String PROBLEM_INFO_FILE = "ProblemInfo.csv";
+	private static final String GET_PROBLEM_FROM_PID_URL = "http://uhunt.felix-halim.net/api/p/id/%d";
+	private static final String GET_PROBLEM_FROM_PNUM_URL = "http://uhunt.felix-halim.net/api/p/num/%d";
 
 	int problemNum;
+	int problemID;
     String category;
 	int level;
 
-	public Problem(int problemNum, String category, int level) {
+	public Problem(int problemNum, int problemID, String category, int level) {
 		this.problemNum = problemNum;
+		this.problemID = problemID;
 		this.category = category;
 		this.level = level;
 	}
 
     @Override
     public String toString() {
-		return "{Problem Num: " + problemNum + ", Category: " + category + ", Level: " + level + "}";
+		return "{Problem Num: " + problemNum + ", Problem ID: " + problemID + ", Category: " + category + ", Level: " + level + "}";
 	}
 
 	public static ArrayList<Problem> getProblems() throws FileNotFoundException {
@@ -33,14 +44,49 @@ public class Problem {
 
 		ArrayList<Problem> problems = new ArrayList<>();
 		while (fileScanner.hasNext()) {
+			int problemNum = fileScanner.nextInt();
 			int problemID = fileScanner.nextInt();
 			String category = fileScanner.next();
 			int level = fileScanner.nextInt();
 
-			problems.add(new Problem(problemID, category, level));
+			problems.add(new Problem(problemNum, problemID, category, level));
 		}
 
 		return problems;
+	}
+
+	public static int getProblemNum(int problemID) throws IOException {
+		URL url = null;
+		try {
+			url = new URL(String.format(GET_PROBLEM_FROM_PID_URL, problemID));
+		} catch (MalformedURLException ex) {
+			Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+		}
+
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod("GET");
+		Scanner responseStream = new Scanner(connection.getInputStream());
+		responseStream.useDelimiter("\\A");
+
+		JSONObject response = new JSONObject(responseStream.next());
+		return response.getInt("num");
+	}
+
+	public static int getProblemID(int problemNum) throws IOException {
+		URL url = null;
+		try {
+			url = new URL(String.format(GET_PROBLEM_FROM_PNUM_URL, problemNum));
+		} catch (MalformedURLException ex) {
+			Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+		}
+
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod("GET");
+		Scanner responseStream = new Scanner(connection.getInputStream());
+		responseStream.useDelimiter("\\A");
+
+		JSONObject response = new JSONObject(responseStream.next());
+		return response.getInt("pid");
 	}
     
 }
--- src/main/java/com/strawhats/problemrecommendation/User.java
+++ src/main/java/com/strawhats/problemrecommendation/User.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -16,7 +15,6 @@ public class User {
 
 	private static final String GET_UID_URL = "http://uhunt.felix-halim.net/api/uname2uid/%s";
 	private static final String GET_USER_SUBMISSIONS_URL = "http://uhunt.felix-halim.net/api/subs-user/%d";
-	private static final String GET_PROBLEM_FROM_PID_URL = "http://uhunt.felix-halim.net/api/p/id/%d";
 
 	public static int getUID(String username) throws IOException {
 		URL url = null;
@@ -61,28 +59,11 @@ public class User {
 		int[] arr = new int[set.size()];
 		int i = 0;
 		for (Integer pid : set) {
-			arr[i] = getProblemNum(pid);
+			arr[i] = Problem.getProblemNum(pid);
 			i++;
 		}
 
 		return arr;
-	}
-
-	private static int getProblemNum(int problemID) throws IOException {
-		URL url = null;
-		try {
-			url = new URL(String.format(GET_PROBLEM_FROM_PID_URL, problemID));
-		} catch (MalformedURLException ex) {
-			Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
-		}
-
-		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-		connection.setRequestMethod("GET");
-		Scanner responseStream = new Scanner(connection.getInputStream());
-		responseStream.useDelimiter("\\A");
-
-		JSONObject response = new JSONObject(responseStream.next());
-		return response.getInt("num");
 	}
 
 }
