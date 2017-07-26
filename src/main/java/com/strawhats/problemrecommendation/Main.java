cc96e97fce407acdc71667e690c633fb1e5fd025
Ahmed Rafiq <ahmedrafiq47@gmail.com>
2017-07-26 13:16:49 +0600

    Can make sugesstions for new users

--- src/main/java/com/strawhats/problemrecommendation/Main.java
+++ src/main/java/com/strawhats/problemrecommendation/Main.java
@@ -6,9 +6,12 @@
 package com.strawhats.problemrecommendation;
 
 import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -16,17 +19,52 @@ import java.util.logging.Logger;
  */
 public class Main {
 
-    public static void main(String[] args) {
-		try {
-			int[] arr = User.getAccepted("holahmeds");
-			for (int i : arr) {
-				System.out.println(i);
+	private static final String[] CATEGORIES = {
+		"Introduction", "Data Structures and Libraries", "Problem Solving Paradigms",
+		"Graph", "Mathematics", "String Processing", "(Computational) Geometry"
+	};
+
+	public static void main(String[] args) {
+		/*
+		HashMap<Integer, Problem> problemNumMap = new HashMap<>();
+		for (Problem p : problems) {
+			problemNumMap.put(p.problemNum, p);
+		}
+
+		ArrayList<HashMap<String, ArrayList<Integer>>> info = new ArrayList<>();
+		for (int i = 0; i < 7; i++) {
+			HashMap<String, ArrayList<Integer>> map = new HashMap<>();
+			for (String cat : CATEGORIES) {
+				map.put(cat, new ArrayList<>());
 			}
+			info.add(map);
+		}
+		for (Problem p : problems) {
+			info.get(p.level).get(p.category).add(p.problemNum);
+		}
+
+		ArrayList<Problem> suggestions = new ArrayList<>();
+		HashMap<String, ArrayList<Integer>> level0Probs = info.get(0);
+		for (String cat : CATEGORIES) {
+			for (int i = 0; i < 2 && i < level0Probs.get(cat).size(); i++) {
+				suggestions.add(problemNumMap.get(level0Probs.get(cat).get(i)));
+			}
+		}
+		 */
+
+		System.out.println(newUserSuggestions());
+	}
+
+	private static List<Problem> newUserSuggestions() {
+		ArrayList<Problem> problems = null;
+		try {
+			problems = Problem.getProblems();
 		} catch (FileNotFoundException ex) {
 			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-		} catch (IOException ex) {
-			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+			System.exit(-1);
 		}
+
+		return problems.stream().filter(problem -> problem.level == 0).collect(Collectors.toList());
 	}
 
 }
