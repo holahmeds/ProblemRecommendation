5bf16c9a75d568ea5949d574e5190b7aa95c820d
Maqsud ul Anwar <maqsud13101033@gmail.com>
2017-08-12 13:27:27 +0600

    Implemented program IO

--- src/main/java/com/strawhats/problemrecommendation/Main.java
+++ src/main/java/com/strawhats/problemrecommendation/Main.java
@@ -6,9 +6,11 @@
 package com.strawhats.problemrecommendation;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -19,40 +21,24 @@ import java.util.stream.Collectors;
  */
 public class Main {
 
-	private static final String[] CATEGORIES = {
-		"Introduction", "Data Structures and Libraries", "Problem Solving Paradigms",
-		"Graph", "Mathematics", "String Processing", "(Computational) Geometry"
-	};
-
 	public static void main(String[] args) {
-		/*
-		HashMap<Integer, Problem> problemNumMap = new HashMap<>();
-		for (Problem p : problems) {
-			problemNumMap.put(p.problemNum, p);
-		}
-
-		ArrayList<HashMap<String, ArrayList<Integer>>> info = new ArrayList<>();
-		for (int i = 0; i < 7; i++) {
-			HashMap<String, ArrayList<Integer>> map = new HashMap<>();
-			for (String cat : CATEGORIES) {
-				map.put(cat, new ArrayList<>());
-			}
-			info.add(map);
-		}
-		for (Problem p : problems) {
-			info.get(p.level).get(p.category).add(p.problemNum);
-		}
-
-		ArrayList<Problem> suggestions = new ArrayList<>();
-		HashMap<String, ArrayList<Integer>> level0Probs = info.get(0);
-		for (String cat : CATEGORIES) {
-			for (int i = 0; i < 2 && i < level0Probs.get(cat).size(); i++) {
-				suggestions.add(problemNumMap.get(level0Probs.get(cat).get(i)));
-			}
-		}
-		 */
-
-		System.out.println(newUserSuggestions());
+            Scanner input = new Scanner(System.in);
+            
+            while (input.hasNext()) {
+                String user = input.next();
+                
+                try {
+                    List<Problem> recProblems = User.getAccepted(user).isEmpty() ? newUserSuggestions() : User.getRecommendedProblems(user);
+                    
+                    System.out.println(user);
+                    for (Problem p : recProblems) {
+                        System.out.println(p);
+                    }
+                    System.out.println();
+                } catch (IOException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
 	}
 
 	private static List<Problem> newUserSuggestions() {
